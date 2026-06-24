6c1b883eafc11cbef523a805b20faeb2207fb9a1
koushalgada <koushalgada@gmail.com>
2026-06-24 13:14:28 +0530

    fix: use JWT auth directly to resolve OpenSSL 3 incompatibility on Node 18+

--- lib/sheets.ts
+++ lib/sheets.ts
@@ -1,4 +1,5 @@
 import { google } from 'googleapis'
+import { JWT } from 'google-auth-library'
 import { Ticket, TicketStatus, TicketPriority } from './types'
 
 const SHEET_ID = process.env.GOOGLE_SHEET_ID!
@@ -11,11 +12,9 @@ const HEADERS = [
 ]
 
 function getAuth() {
-  return new google.auth.GoogleAuth({
-    credentials: {
-      client_email: process.env.GOOGLE_CLIENT_EMAIL,
-      private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    },
+  return new JWT({
+    email: process.env.GOOGLE_CLIENT_EMAIL,
+    key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
   })
 }
